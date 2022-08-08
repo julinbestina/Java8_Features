df35b5142bd958f0b2f007c955f55ded8797f605
julinbestina <julin.bestina@yahoo.com>
2022-08-08 08:29:55 +0530

    Added code for Stream and Lambda Expression

---
+++ src/com/bl/java8/FunInterfaceDemo.java
@@ -0,0 +1,24 @@
+package com.bl.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FunInterfaceDemo {
+
+	public static void main(String[] args) {
+
+		Predicate<Integer> isEven =(n)->n%2==0;
+		System.out.println("Result = "+ isEven.test(59));
+		
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(4);
+		numbers.add(5);
+		numbers.add(6);
+		numbers.add(7);
+		numbers.add(8);
+		
+		numbers.stream().filter(isEven).forEach(n->System.out.println("Even = "+ n));
+	}
+
+}
